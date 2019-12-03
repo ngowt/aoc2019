511f17efc45a2cc7e441900c2fc637016ee8db36
Wilton Ngo <wiltonngo@gmail.com>
2019-12-03 02:07:32 -0800

    add default value for total

--- d1/b.js
+++ d1/b.js
@@ -6,7 +6,7 @@ const fuel = (mass) => {
     return res > 0 ? res : 0;
 };
 
-const fuelSeries = (f, total) => {
+const fuelSeries = (f, total = 0) => {
     while (f > 0) {
       total += fuel(f);
       f = fuel(f);
@@ -15,7 +15,7 @@ const fuelSeries = (f, total) => {
   }
   
 const answer = input.reduce((acc, cv) => {
-    return acc += fuelSeries(cv, 0);
+    return acc += fuelSeries(cv);
 }, 0)
 
 console.log(answer); //4943923
